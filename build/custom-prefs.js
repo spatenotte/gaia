7750a54279087434cca6f820edb75944317eea35
Sammy Patenotte <sammy.patenotte@yahoo.com>
2015-08-08 09:18:17 -0700

    Added custom-prefs.js

---
+++ build/custom-prefs.js
@@ -0,0 +1,8 @@
+{
+  "lockscreen.enabled": false,
+  "lockscreen.locked": false,
+  "devtools.debugger.remote-enabled": true,
+  "debu.gaia.enabled": true,
+  "devtools.unrestricted": true,
+  "sceen.timeout": 360
+}
