1c56a73dffea8186100b8f7654b3e1f356531829
Sammy Patenotte <sammy.patenotte@yahoo.com>
2015-08-11 16:16:23 -0700

    Removed custom-prefs.js

--- build/custom-prefs.js
+++
@@ -1,8 +0,0 @@
-{
-  "lockscreen.enabled": false,
-  "lockscreen.locked": false,
-  "devtools.debugger.remote-enabled": true,
-  "debu.gaia.enabled": true,
-  "devtools.unrestricted": true,
-  "sceen.timeout": 360
-}
